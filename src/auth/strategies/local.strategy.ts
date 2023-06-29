de54c214147e4b22f86466ed67d9d13528feefe4
Bravo1B9 <brofe93@gmail.com>
2023-06-29 07:08:36 +0000

    Create local.strategy.ts

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,21 +1,34 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from '../dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private jwtService: JwtService) {
     super({
       usernameField: 'email',
       passwordField: 'password'
     })
   };
 
-  async validate() {
-    
+  async validate(dto: LoginDto) {
+    const user = await this.userService.getByEmail(dto.email);
+    const passwordsMatch = await bcrypt.compare(dto.password, user.hash);
+    if(!user) {
+      throw new UnauthorizedException();
+    };
+    if(!passwordsMatch) {
+      throw new UnauthorizedException();
+    };
+    const payload = { email: dto.email, sub: user.id };
+    return {
+      access_token: await this.jwtService.sign(payload)
+    };
   };
 
 };
