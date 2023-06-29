375f7a14b7b1a30101a4982ced0d2d579fcd559f
Bravo1B9 <brofe93@gmail.com>
2023-06-29 06:12:47 +0000

    Initial register user functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterUserDto } from './dtos';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/user/dtos';
 
 @Injectable()
-export class AuthService {};
+export class AuthService {
+
+  constructor(private userService: UserService) {};
+
+  async register(dto: RegisterUserDto) {
+    const hash = await bcrypt.hash(dto.password, 10);
+    const newUser = new CreateUserDto();
+    newUser.email = dto.email;
+    newUser.hash = hash;
+    newUser.firstName = dto.firstName;
+    newUser.lastName = dto.lastName;
+    await this.userService.create(newUser);
+  };
+
+};
