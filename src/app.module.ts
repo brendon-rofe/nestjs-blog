d4454b77859d29958af38dc39ca934fd5e308090
Bravo1B9 <brofe93@gmail.com>
2023-07-02 15:22:29 +0000

    Initial comments module, controller and service setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule, PostsModule],
+  imports: [DatabaseModule, UserModule, AuthModule, PostsModule, CommentsModule],
 })
 export class AppModule {};
