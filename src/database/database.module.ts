cd22f54e7bff12ef22bef93aa4850ad0d05dfab3
Bravo1B9 <brofe93@gmail.com>
2023-07-03 10:39:51 +0000

    Import comment entity, where necissary

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { CommentEntity } from 'src/comments/comment.entity';
 import { PostEntity } from 'src/posts/post.entity';
 import { UserEntity } from 'src/user/user.entity';
 config();
@@ -14,7 +15,7 @@ config();
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [UserEntity, PostEntity],
+      entities: [UserEntity, PostEntity, CommentEntity],
       synchronize: true,
     }),
   ],
