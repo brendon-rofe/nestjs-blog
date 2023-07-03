4a65b76f493ec68f315a0a40f5c6290c81173f67
Bravo1B9 <brofe93@gmail.com>
2023-07-03 10:45:11 +0000

    Initial comments controller setup

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { CommentsService } from './comments.service';
 
 @Controller('comments')
-export class CommentsController {};
+export class CommentsController {
+
+  constructor(private commentsService: CommentsService) {};
+
+  @Post()
+  async create() {};
+
+  @Get()
+  async getById() {};
+
+  @Patch()
+  async update() {};
+
+  @Delete()
+  async delete() {};
+
+};
