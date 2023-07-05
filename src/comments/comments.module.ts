c0a2425ad606c7f62ec1d833d650e745e2591ccc
Bravo1B9 <brofe93@gmail.com>
2023-07-05 09:12:53 +0000

    Import UserModule

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -4,9 +4,10 @@ import { CommentsService } from './comments.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './comment.entity';
 import { PostsModule } from 'src/posts/posts.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity]), PostsModule],
+  imports: [TypeOrmModule.forFeature([CommentEntity]), PostsModule, UserModule],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
